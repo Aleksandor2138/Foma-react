db2c51048a426f6872fef3ba6903674b0591349e
Aleksandor2138 <99901672+Aleksandor2138@users.noreply.github.com>
2023-06-10 21:58:36 +0300

    проверка деплоя

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="Foma-react">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>
