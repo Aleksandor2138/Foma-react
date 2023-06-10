1b7fbd1bbd62092b83ca73c3c41a16aa05a5539e
Aleksandor2138 <99901672+Aleksandor2138@users.noreply.github.com>
2023-06-10 22:20:47 +0300

    повтор деплоя

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
-import { StrictMode } from 'react';
+import React from 'react';
+import { Provider } from 'react-redux';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { store, persistor } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import App from 'components/App';
 import './index.css';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>
+  <React.StrictMode>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </BrowserRouter>
+    </PersistGate>
+  </React.StrictMode>
 );
